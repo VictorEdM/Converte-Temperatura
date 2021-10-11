d30b868865607ed757ccce72fd85955b5f48aa0c
VictorEdM <thomasmannautor@gmail.com>
2021-10-11 16:03:36 -0300

    Type hint corrigido

--- temperatura.py
+++ temperatura.py
@@ -17,63 +17,63 @@ class Temperatura(object):
     # ------------------------------------------------------------------
     
     def celsius_para_fahrenheit(self) -> None:
-        self.temperatura_saida: float = float(
+        self.temperatura_saida = float(
             f"{self.temperatura_entrada * 1.8 + 32:.2f}"
         )
 
     # ------------------------------------------------------------------
 
     def celsius_para_kelvin(self) -> None:
-        self.temperatura_saida: float = float(
+        self.temperatura_saida = float(
             f"{self.temperatura_entrada + 273:.2f}"
         )
 
     # ------------------------------------------------------------------
     
     def celsius_para_celsius(self) -> None:
-        self.temperatura_saida: float = float(
+        self.temperatura_saida = float(
             f"{self.temperatura_entrada:.2f}"
         )
     
     # ------------------------------------------------------------------
 
     def fahrenheit_para_celsius(self) -> None:
-        self.temperatura_saida: float = float(
+        self.temperatura_saida = float(
             f"{(self.temperatura_entrada - 32) / 1.8:.2f}"
         )
 
     # ------------------------------------------------------------------
 
     def fahrenheit_para_kelvin(self) -> None:
-        self.temperatura_saida: float = float(
+        self.temperatura_saida = float(
             f"{(self.temperatura_entrada - 32) * (5/9) + 273:.2f}"
         )
 
     # ------------------------------------------------------------------
     
     def fahrenheit_para_fahrenheit(self) -> None:
-        self.temperatura_saida: float = float(
+        self.temperatura_saida = float(
             f"{self.temperatura_entrada:.2f}"
         )
     
     # ------------------------------------------------------------------
 
     def kelvin_para_celsius(self) -> None:
-        self.temperatura_saida: float = float(
+        self.temperatura_saida = float(
             f"{self.temperatura_entrada - 273:.2f}"
         )
 
     # ------------------------------------------------------------------
 
     def kelvin_para_fahrenheit(self) -> None:
-        self.temperatura_saida: float = float(
+        self.temperatura_saida = float(
             f"{(self.temperatura_entrada - 273) * 1.8 + 32:.2f}"
         )
 
     # ------------------------------------------------------------------
     
     def kelvin_para_kelvin(self) -> None:
-        self.temperatura_saida: float = float(
+        self.temperatura_saida = float(
             f"{self.temperatura_entrada:.2f}"
         )
     
